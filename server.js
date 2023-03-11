e5fab32591c3ea965ae1115ee11c3f37a6186352
Riya Sharma <theriyasharma24@gmail.com>
2023-03-11 21:29:33 +0530

    Add server

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+var cors = require("cors");
+
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
