b783e1cd684b6b723e8f00ab1468190d95100cba
Riya Sharma <theriyasharma24@gmail.com>
2023-07-30 12:51:47 +0530

    update localhost port

--- server.js
+++ server.js
@@ -72,6 +72,6 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
